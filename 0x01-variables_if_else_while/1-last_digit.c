4207ca3422b8cf0f0173ea663e5833f88cbfea2d
amdomina <amdomg2016@gmail.com>
2023-03-17 20:10:24 -0700

    last gigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -6,16 +7,15 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-int n;
+{int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d and is ", n, abs(n % 10));
+printf("Last digit of %d is %d ", n, abs(n % 10));
 if (abs(n % 10) > 5)
-printf("greater than 5\n");
+printf("and is greater than 5\n");
 else if (abs(n % 10) == 0)
-printf("0\n");
+printf("and is 0\n");
 else
-printf("less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n");
 return (0);
 }
