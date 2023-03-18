308cb01942dd911446a7f99abd3d18511323594d
amdomina <amdomg2016@gmail.com>
2023-03-18 03:42:32 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,16 @@
 /**
 * main - starting point for the programe
 *
-*Return: Always 0 (success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		printf("%d", i);
+	}
+	printf("%d");
 	return (0);
 }
