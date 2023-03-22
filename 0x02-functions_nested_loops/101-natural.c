1dbda5626ee99e390361dafd631eb29ece21aa6c
amdomina <amdomg2016@gmail.com>
2023-03-21 22:40:15 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 
 	while (i < 1024)
 	{
-		if ((i % 3) || (i % 5 == 0))
-		{
-			z += i;
-		}
-		i++;
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
 	}
 	printf("%d\n", z);
 	return (0);
