a19db4c12fd8544ebd0ee5a1846802108ef431d5
amdomina <amdomg2016@gmail.com>
2023-03-23 12:23:08 -0700

    add one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,13 @@ int main(void)
 	int i;
 
 	printf("infinite loop incoming :(\n");
-	i = 0;
-	/*while (i < 10)*/
-	/*{*/
-	/*	putchar(i);*/
-	/*}*/
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+
 	printf("infinite loop avoided! \\o/\n");
+
 	return (0);
 }
